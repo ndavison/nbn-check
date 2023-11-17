e0aecd0d572412a2f1876e4ccccd3b582300f898
Nathan Davison <1152676+ndavison@users.noreply.github.com>
2023-11-17 22:36:36 +1100

    Remove the email parts of the workflow, and instead fail the workflow when the changed condition is met, to use github notifications as a means to emailing when NBN data has changed

--- nbn-check.py
+++ nbn-check.py
@@ -1,6 +1,6 @@
 import requests
 import os
-import json
+import sys
 
 url = f'https://places.nbnco.net.au/places/v2/details/{os.environ["LOC_ID"]}'
 r = requests.get(url, headers={'referer': 'https://www.nbnco.com.au/'})
@@ -8,5 +8,9 @@ if r.ok:
     resp = r.json()
     addr = resp.get('addressDetail', {})
     if addr.get('techChangeStatus', '').lower() == 'planned':
-        with open('results.json', 'w') as f:
-            f.write(json.dumps(resp))
+        out = {'reasonCode': addr.get('reasonCode'),
+               'altReasonCode': addr.get('altReasonCode'),
+               'techChangeStatus': addr.get('techChangeStatus'),
+               'previousTechChangeStatus': addr.get('previousTechChangeStatus')}
+        print(out)
+        sys.exit(1)
