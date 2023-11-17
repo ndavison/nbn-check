0f705be55724f3a0364d07c16c08401f3b412779
Nathan Davison <1152676+ndavison@users.noreply.github.com>
2023-11-17 23:14:56 +1100

    Restore the correct tech change status condition being checked

--- nbn-check.py
+++ nbn-check.py
@@ -7,7 +7,7 @@ r = requests.get(url, headers={'referer': 'https://www.nbnco.com.au/'})
 if r.ok:
     resp = r.json()
     addr = resp.get('addressDetail', {})
-    if addr.get('techChangeStatus', '').lower() == 'planned':
+    if addr.get('techChangeStatus', '').lower() != 'planned':
         out = {'reasonCode': addr.get('reasonCode'),
                'altReasonCode': addr.get('altReasonCode'),
                'techChangeStatus': addr.get('techChangeStatus'),
